1a7a44cb61f8f670c45dfddd3c209799eaa604cc
mikhail-ram <mikhailethanram@gmail.com>
2025-03-23 00:17:17 +0530

    Added custom mltk_data path

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import logging
 import nltk
 import spacy
+import os
 
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+# Add this directory to nltk's data path
+nltk.data.path.append(nltk_data_dir)
+# Download the required model to the new directory
 nltk.download('punkt', quiet=True)
 nltk.download('averaged_perceptron_tagger', quiet=True)
 nltk.download('stopwords', quiet=True)
